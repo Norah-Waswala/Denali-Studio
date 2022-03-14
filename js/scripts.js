5c838cd37c52a889f87cabef2e8de28953215f55
Norah.Waswala <norah.waswala@student.moringa.com>
2022-03-14 12:13:51 +0300

    add css styles, padding and margin to welcome section

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 function myFunction() {
     var design = document.getElementById("designer");
-    // design.onclick = function(event) {
-    //     console.log (event);
+    design.onclick = function(event) {
+        console.log (event)
     }
     if (design.style.display === "none") {
     } else {
       design.style.display = "none";
     }
     document.getElementById("designer").innerHTML=Event
-  
+// function myMessage() {
+//   alert("thanks for the feedback")
+//   document.getElementsByClassName("message").innerHTML= myMessage
+  }
