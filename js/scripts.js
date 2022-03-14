487a3a53a35ef14e5e332558be640221271c1771
Norah.Waswala <norah.waswala@student.moringa.com>
2022-03-14 12:44:31 +0300

    add padding and margin to about us section

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,33 @@ function myFunction() {
 //   alert("thanks for the feedback")
 //   document.getElementsByClassName("message").innerHTML= myMessage
   }
+
+
+  function myDesign(){
+    var des = document.getElementById("designer");
+  }
+  if (des.style.display == "none"){
+    des.style.display = "block";
+  }
+
+
+
+
+
+
+
+  function myGallery() {
+    var x = document.getElementById("slideshow");
+    var y = document.getElementById("slideshow2");
+    var w = document.getElementById("slideshow3");
+    var z = document.getElementById("slideshow4");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        w.style.display = "none";
+        z.style.display = "none";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
