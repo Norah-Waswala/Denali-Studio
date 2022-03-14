f0c6c609fbde0305f3ed7a78f6a546557d4b65ff
Norah.Waswala <norah.waswala@student.moringa.com>
2022-03-14 18:04:53 +0300

    hover effect on portfolio

--- js/jquery.js
+++ js/jquery.js
@@ -20,5 +20,90 @@ $(document).ready(function(){
 
 
 // Hover Effect
+// Img 1
 
+$("#img1").mouseenter(function(){
+    $(this).css("opacity", "0.3");
+    $(this).text("Project 1");
+});
+$("#img1").mouseleave(function(){
+    $(this).css("opacity", "0.7");
+    $(this).off();
+});
 
+// Img 2
+
+$("#img2").mouseenter(function(){
+    $(this).css("opacity", "0.3");
+    $(this).text("Project 2");
+});
+$("#img2").mouseleave(function(){
+    $(this).css("opacity", "0.7");
+    $(this).show();
+});
+
+// Img 3
+
+$("#img3").mouseenter(function(){
+    $(this).css("opacity", "0.3");
+    $(this).text("Project 3");
+});
+$("#img3").mouseleave(function(){
+    $(this).css("opacity", "0.7");
+    $(this).show();
+});
+
+// Img 4
+
+$("#img4").mouseenter(function(){
+    $(this).css("opacity", "0.3");
+    $(this).text("Project 4");
+});
+$("#img4").mouseleave(function(){
+    $(this).css("opacity", "0.7");
+    $(this).show();
+});
+
+// Img 5
+
+$("#img5").mouseenter(function(){
+    $(this).css("opacity", "0.3");
+    $(this).text("Project 5");
+});
+$("#img5").mouseleave(function(){
+    $(this).css("opacity", "0.7");
+    $(this).show();
+});
+
+// Img 6
+
+$("#img6").mouseenter(function(){
+    $(this).css("opacity", "0.3");
+    $(this).text("Project 6");
+});
+$("#img6").mouseleave(function(){
+    $(this).css("opacity", "0.7");
+    $(this).show();
+});
+
+// Img 7
+
+$("#img7").mouseenter(function(){
+    $(this).css("opacity", "0.3");
+    $(this).text("Project 7");
+});
+$("#img7").mouseleave(function(){
+    $(this).css("opacity", "0.7");
+    $(this).show();
+});
+
+// Img 8
+
+$("#img8").mouseenter(function(){
+    $(this).css("opacity", "0.3");
+    $(this).text("Project 8");
+});
+$("#img8").mouseleave(function(){
+    $(this).css("opacity", "0.7");
+    $(this).show();
+});
