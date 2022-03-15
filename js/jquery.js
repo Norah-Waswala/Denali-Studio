742838b628bec538715ad464172264a3b8c75d08
Norah.Waswala <norah.waswala@student.moringa.com>
2022-03-15 11:43:47 +0300

    add css styles to gallery

--- js/jquery.js
+++ js/jquery.js
@@ -1,20 +1,20 @@
 // Design click function
 
 $(document).ready(function() {
-    $("img").click(function() {
-      $("p#design").toggle();
+    $(".designImg").click(function() {
+      $("#design").toggle();
     });
 });
 
 $(document).ready(function(){
-    $("img").click(function(){
-      $(".toggle#dev").toogle();
+    $(".devImg").click(function(){
+      $("#dev").toogle();
     });
 });
 
 $(document).ready(function(){
-    $("img").click(function(){
-      $(".toggle#dev").toogle();
+    $(".manImg").click(function(){
+      $("#product-m").toogle();
     });
 });
 
@@ -22,88 +22,88 @@ $(document).ready(function(){
 // Hover Effect
 // Img 1
 
-$("#img1").mouseenter(function(){
-    $(this).css("opacity", "0.3");
-    $(this).text("Project 1");
-});
-$("#img1").mouseleave(function(){
-    $(this).css("opacity", "0.7");
-    $(this).off();
-});
+// $("#img1").mouseenter(function(){
+//     $(this).css("opacity", "0.3");
+//     $(this).text("Project 1");
+// });
+// $("#img1").mouseleave(function(){
+//     $(this).css("opacity", "0.7");
+//     $("#img1").unbind('mouseenter mouseleave');
+// });
 
-// Img 2
+// // Img 2
 
-$("#img2").mouseenter(function(){
-    $(this).css("opacity", "0.3");
-    $(this).text("Project 2");
-});
-$("#img2").mouseleave(function(){
-    $(this).css("opacity", "0.7");
-    $(this).show();
-});
+// $("#img2").mouseenter(function(){
+//     $(this).css("opacity", "0.3");
+//     $(this).text("Project 2");
+// });
+// $("#img2").mouseleave(function(){
+//     $(this).css("opacity", "0.7");
+//     $(this).show();
+// });
 
-// Img 3
+// // Img 3
 
-$("#img3").mouseenter(function(){
-    $(this).css("opacity", "0.3");
-    $(this).text("Project 3");
-});
-$("#img3").mouseleave(function(){
-    $(this).css("opacity", "0.7");
-    $(this).show();
-});
+// $("#img3").mouseenter(function(){
+//     $(this).css("opacity", "0.3");
+//     $(this).text("Project 3");
+// });
+// $("#img3").mouseleave(function(){
+//     $(this).css("opacity", "0.7");
+//     $(this).show();
+// });
 
-// Img 4
+// // Img 4
 
-$("#img4").mouseenter(function(){
-    $(this).css("opacity", "0.3");
-    $(this).text("Project 4");
-});
-$("#img4").mouseleave(function(){
-    $(this).css("opacity", "0.7");
-    $(this).show();
-});
+// $("#img4").mouseenter(function(){
+//     $(this).css("opacity", "0.3");
+//     $(this).text("Project 4");
+// });
+// $("#img4").mouseleave(function(){
+//     $(this).css("opacity", "0.7");
+//     $(this).show();
+// });
 
-// Img 5
+// // Img 5
 
-$("#img5").mouseenter(function(){
-    $(this).css("opacity", "0.3");
-    $(this).text("Project 5");
-});
-$("#img5").mouseleave(function(){
-    $(this).css("opacity", "0.7");
-    $(this).show();
-});
+// $("#img5").mouseenter(function(){
+//     $(this).css("opacity", "0.3");
+//     $(this).text("Project 5");
+// });
+// $("#img5").mouseleave(function(){
+//     $(this).css("opacity", "0.7");
+//     $(this).show();
+// });
 
-// Img 6
+// // Img 6
 
-$("#img6").mouseenter(function(){
-    $(this).css("opacity", "0.3");
-    $(this).text("Project 6");
-});
-$("#img6").mouseleave(function(){
-    $(this).css("opacity", "0.7");
-    $(this).show();
-});
+// $("#img6").mouseenter(function(){
+//     $(this).css("opacity", "0.3");
+//     $(this).text("Project 6");
+// });
+// $("#img6").mouseleave(function(){
+//     $(this).css("opacity", "0.7");
+//     $(this).show();
+// });
 
-// Img 7
+// // Img 7
 
-$("#img7").mouseenter(function(){
-    $(this).css("opacity", "0.3");
-    $(this).text("Project 7");
-});
-$("#img7").mouseleave(function(){
-    $(this).css("opacity", "0.7");
-    $(this).show();
-});
+// $("#img7").mouseenter(function(){
+//     $(this).css("opacity", "0.3");
+//     $(this).text("Project 7");
+// });
+// $("#img7").mouseleave(function(){
+//     $(this).css("opacity", "0.7");
+//     $(this).show();
+// });
 
-// Img 8
+// // Img 8
 
-$("#img8").mouseenter(function(){
-    $(this).css("opacity", "0.3");
-    $(this).text("Project 8");
-});
-$("#img8").mouseleave(function(){
-    $(this).css("opacity", "0.7");
-    $(this).show();
-});
+// $("#img8").mouseenter(function(){
+//     $(this).css("opacity", "0.3");
+//     $(this).text("Project 8");
+// });
+// $("#img8").mouseleave(function(){
+//     $(this).css("opacity", "0.7");
+//     $(this).show();
+// });
